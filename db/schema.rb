96c29c3a79218d96f4572451dca1188f81cf659d
= <=>
2018-05-14 19:53:07 +1000

    added state datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180429012051) do
+ActiveRecord::Schema.define(version: 20180514090346) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180429012051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "status"
+    t.datetime "start"
   end
 
   create_table "users", force: :cascade do |t|
